e2048356434f9d87f60a58b43fc19c2880227c8b
SimiCloudSec <similoluwatalabi@gmail.com>
2025-12-20 11:54:42 -0800

    Add Jenkinsfile-1

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,38 +1,43 @@
-##Install the needed packages and enable the services(MariaDb, Apache)
-sudo yum update -y
-sudo yum install git -y
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-sudo yum install -y httpd mariadb-server
+#!/bin/bash
+set -e
+
+## Install packages for Amazon Linux 2023
+sudo dnf update -y
+sudo dnf install -y git
+sudo dnf install -y httpd
+sudo dnf install -y php php-mysqli php-json php-gd php-mbstring php-xml
+sudo dnf install -y mariadb105-server
+sudo dnf install -y nfs-utils amazon-efs-utils
+
+## Start and enable Apache
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo systemctl is-enabled httpd
 
-##Add ec2-user to Apache group and grant permissions to /var/www
+## Start and enable MariaDB
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+## Add ec2-user to Apache group and grant permissions to /var/www
 sudo usermod -a -G apache ec2-user
 sudo chown -R ec2-user:apache /var/www
 sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
 find /var/www -type f -exec sudo chmod 0664 {} \;
-cd /var/www/html
 
+## Update httpd.conf
 sudo sed -i '151s/None/All/' /etc/httpd/conf/httpd.conf
 
-##Grant file ownership of /var/www & its contents to apache user
+## Grant file ownership of /var/www & its contents to apache user
 sudo chown -R apache /var/www
-
-##Grant group ownership of /var/www & contents to apache group
 sudo chgrp -R apache /var/www
-
-##Change directory permissions of /var/www & its subdir to add group write 
 sudo chmod 2775 /var/www
 find /var/www -type d -exec sudo chmod 2775 {} \;
-
-##Recursively change file permission of /var/www & subdir to add group write perm
 sudo find /var/www -type f -exec sudo chmod 0664 {} \;
 
-##Restart Apache
+## Restart Apache
 sudo systemctl restart httpd
-sudo service httpd restart
 
-##Enable httpd
-sudo systemctl enable httpd
+## TCP keepalive settings
 sudo /sbin/sysctl -w net.ipv4.tcp_keepalive_time=200 net.ipv4.tcp_keepalive_intvl=200 net.ipv4.tcp_keepalive_probes=5
+
+echo "=== Setup Complete ==="
