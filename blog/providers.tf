d7a8971402866f894b48c473dae6708f25364b1c
SimiCloudSec <similoluwatalabi@gmail.com>
2025-12-22 01:56:58 -0800

    Deploy Blog to Management account

--- blog/providers.tf
+++ blog/providers.tf
@@ -1,9 +1,24 @@
 terraform {
   required_version = ">= 1.0"
   required_providers {
-    aws   = { source = "hashicorp/aws", version = "~> 5.0" }
-    tls   = { source = "hashicorp/tls", version = "~> 4.0" }
-    local = { source = "hashicorp/local", version = "~> 2.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
 }
-provider "aws" { region = var.aws_region }
+
+provider "aws" {
+  region = var.aws_region
+  assume_role {
+    role_arn = "arn:aws:iam::227764537934:role/TerraformExecutionRole"
+  }
+}
