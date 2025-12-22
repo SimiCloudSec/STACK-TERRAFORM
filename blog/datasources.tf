bc9eabeccec07d39ee20f262fa81b8ac797f197d
SimiCloudSec <similoluwatalabi@gmail.com>
2025-12-22 01:33:45 -0800

    Fix datasources.tf filter syntax

--- blog/datasources.tf
+++ blog/datasources.tf
@@ -1,6 +1,14 @@
 data "aws_ami" "golden_ami" {
   most_recent = true
   owners      = ["289390529512"]
-  filter { name = "name", values = ["ami-stack-*"] }
-  filter { name = "virtualization-type", values = ["hvm"] }
+
+  filter {
+    name   = "name"
+    values = ["ami-stack-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
